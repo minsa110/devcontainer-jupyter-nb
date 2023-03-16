10fff896e792ebb4d3722cd5d8d9a8f32911c74a
minsa110 <minsa110@gmail.com>
2023-03-16 09:20:09 -0700

    reduce features, install ipykernel through yml

--- .devcontainer/postcreate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-conda init
-conda install -y -n base ipykernel --update-deps --force-reinstall
-# Uncomment this section to install additional packages from requirements.txt.
-# pip install -r requirements.txt
