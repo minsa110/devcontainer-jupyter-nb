e618da4a24140f5453ca744fae3a7195af52ac67
minsa110 <minsa110@gmail.com>
2023-03-17 13:23:53 -0700

    silently install solver

--- .devcontainer/update-env.sh
+++ .devcontainer/update-env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 conda init
-conda install -n base conda-libmamba-solver
+conda install -n base conda-libmamba-solver -y
 conda config --set solver libmamba
 if [ -f environment.yml* ]; then conda env create -f environment.yml; fi
 conda activate myenv
