b257504cbf63957f47df96ce61dad23e9def15f8
minsa110 <minsa110@gmail.com>
2023-03-17 13:27:23 -0700

    dont activate myenv

--- .devcontainer/update-env.sh
+++ .devcontainer/update-env.sh
@@ -4,4 +4,3 @@ conda init
 conda install -n base conda-libmamba-solver -y
 conda config --set solver libmamba
 if [ -f environment.yml* ]; then conda env create -f environment.yml; fi
-conda activate myenv
